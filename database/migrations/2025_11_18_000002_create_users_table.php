590f89b4f84f88e127b5a48a34484dcb23ef21df
Vikzdimension <vivek.lode1@gmail.com>
2025-11-18 02:13:25 +0530

    fix: clean up and reorder core migrations for organizations, users, roles, and pivot table

--- database/migrations/2025_11_18_000002_create_users_table.php
+++ database/migrations/2025_11_18_000002_create_users_table.php
@@ -11,10 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
+
+            // foreign key to organizations table
+            $table->foreignId('organization_id')->nullable()->constrained()->nullOnDelete();
+
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+
+            $table->rememberToken();
             $table->timestamps();
         });
+
     }
 
     /**
@@ -22,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('users');
     }
 };
